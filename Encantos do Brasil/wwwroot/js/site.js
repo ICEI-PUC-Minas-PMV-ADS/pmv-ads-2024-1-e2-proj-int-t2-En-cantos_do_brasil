664c283f05a9e76e5c5c3f2c207ce31589e026a7
Luiza <luizamariamachado2204@gmail.com>
2024-05-09 22:18:21 -0300

    css, e js da home

--- Encantos do Brasil/wwwroot/js/site.js
+++ Encantos do Brasil/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    var container = document.querySelector('.container');
+    var items = container.querySelectorAll('.item');
+
+    for (var i = 0; i < items.length; i += 4) {
+        var group = Array.prototype.slice.call(items, i, i + 4);
+        var row = document.createElement('div');
+        row.classList.add('row');
+
+        group.forEach(function (item) {
+            row.appendChild(item);
+        });
+
+        container.appendChild(row);
+    }
+});
