15dee5c2e4d8d33760b482d583fafa6ec5f74d72
Saulo <saulolima.c@protonmail.com>
2024-05-22 21:04:02 -0300

    Implementações comentario - inicio

---
+++ Encantos do Brasil/Controllers/ComentariosController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Encantos_do_Brasil.Models;
+
+namespace Encantos_do_Brasil.Controllers
+{
+    public class ComentariosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ComentariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Comentarios
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Comentarios.Include(c => c.Cidade);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Comentarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Comentarios == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentarios
+                .Include(c => c.Cidade)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentario);
+        }
+
+        // GET: Comentarios/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCidade"] = new SelectList(_context.Cidades, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Comentarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(string texto, int idCidade)
+        {
+            var comentario = new Comentario
+            {
+                Texto = texto,
+                IdCidade = idCidade
+            };
+
+            if (ModelState.IsValid)
+            {
+                
+                _context.Add(comentario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(comentario);
+        }
+
+        // GET: Comentarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Comentarios == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentarios.FindAsync(id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCidade"] = new SelectList(_context.Cidades, "Id", "Nome", comentario.IdCidade);
+            return View(comentario);
+        }
+
+        // POST: Comentarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Comentarioss,IdCidade")] Comentario comentario)
+        {
+            if (id != comentario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comentario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComentarioExists(comentario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCidade"] = new SelectList(_context.Cidades, "Id", "Nome", comentario.IdCidade);
+            return View(comentario);
+        }
+
+        // GET: Comentarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Comentarios == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentarios
+                .Include(c => c.Cidade)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentario);
+        }
+
+        // POST: Comentarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Comentarios == null)
+            {
+                return Problem("Entity set 'AppDbContext.Comentarios'  is null.");
+            }
+            var comentario = await _context.Comentarios.FindAsync(id);
+            if (comentario != null)
+            {
+                _context.Comentarios.Remove(comentario);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComentarioExists(int id)
+        {
+          return _context.Comentarios.Any(e => e.Id == id);
+        }
+    }
+}
