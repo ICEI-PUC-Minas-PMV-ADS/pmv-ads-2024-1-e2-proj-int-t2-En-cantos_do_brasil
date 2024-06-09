894bae9c62120c66dd46ab0a724507b7cc07e828
MARCELLY MAIA <144232641+MarcellyPUC@users.noreply.github.com>
2024-06-09 13:04:06 -0300

    estado

--- Encantos do Brasil/ViewModels/CidadeEstadoViewModel.cs
+++ Encantos do Brasil/ViewModels/CidadeEstadoViewModel.cs
@@ -9,21 +9,11 @@ namespace Encantos_do_Brasil.ViewModels
         {
             ImagemEstados = new List<ImagemEstado>();
             ImagemCidades = new List<ImagemCidade>();
-            Cidade = new List<Cidade>();
-            Estado = new List<Estado>();
-            PontoTuristico = new List<PontoTuristico>();
-            Hotel =new List<Hotel>();
-            TextoEstado = new List<TextoEstado>();
-            TextoCidade = new List<TextoCidade>();
+          
         }
 
         public List<ImagemEstado> ImagemEstados { get; set; }
         public List<ImagemCidade> ImagemCidades { get; set; }
-        public List<Cidade> Cidade { get; set; }
-        public List<Estado> Estado { get; set; }
-        public List<PontoTuristico> PontoTuristico { get; set; }
-        public List<Hotel> Hotel { get; set; }
-        public List<TextoEstado> TextoEstado { get; set; }
-        public List<TextoCidade> TextoCidade { get; set; }
+    
     }
 }
